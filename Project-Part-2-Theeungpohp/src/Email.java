01e5e1e2b576f034710c5ee26bd56742a2ef23db
DanielJohnson <djj0007@uah.edu>
2017-02-26 18:53:50 -0600

    added Email/Mailbox outline and UML

--- Project-Part-2-Theeungpohp/src/Email.java
+++ Project-Part-2-Theeungpohp/src/Email.java
@@ -1,4 +1,23 @@
-
-public class Email {
-
+public class Email 
+{
+	private String subject;
+	private String message;
+	
+	/* are these necessary?
+	private String sender;
+	private String receiver;
+	*/
+	
+	public Email(String subject, String message /* , String sender, String Receiver */)
+	{
+		this.subject = subject;
+		this.message = message;
+		//this.sender = sender;
+		//this.receiver = receiver;
+	}
+	
+	public String getSubject(){ return subject;}
+	public String getMessage(){ return message;}
+	//public String getSender() { return sender;}
+	//public String getReceiver(){ return receiver;}
 }
--- Project-Part-2-Theeungpohp/src/Mailbox.java
+++ Project-Part-2-Theeungpohp/src/Mailbox.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 
-public class Mailbox {
-
+public class Mailbox 
+{
+	private ArrayList<Email> emails = new ArrayList<Email>();
+	
+	public Mailbox()
+	{
+		
+	}
+	
+	// Skeleton Methods
+	public void getEmail(int index){}
+	public void addEmail(Email email){}
+	public void deleteEmail(int index){}
 }
