e72d1cd5fa960c798941051d8b69483f24e1c8fa
wh0015 <uah.edu>
2017-04-12 15:00:51 -0500

    more o' that good shit

--- Project-Part-3-Theeungpohp/src/JUnitTestUser.java
+++ Project-Part-3-Theeungpohp/src/JUnitTestUser.java
@@ -2,6 +2,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * 
+ * @author Will Hildreth
+ * JUnit test for User
+ * 4/12/2017
+ *
+ */
 public class JUnitTestUser
 {
 
@@ -30,6 +37,46 @@ public class JUnitTestUser
 		//get email works
 		e = a.getEmail(0,0,0,0);
 		assertTrue(e == null);
+	}
+	
+	@Test
+	public void TestDeleteEmail()
+	{
+		User u = new User("test user");
+		Email e = new Email("Hi", "test@gmail.com", "test2@gmail.com");
+		u.addAccount("test2@gmail.com", 0);
+		u.addAccount("test2@gmail.com", 1);
+		
+		u.sendEmail(e);
+		assertTrue(u.deleteEmail(0, 0, 0, 1) == null);
+		assertTrue(u.deleteEmail(0, 0, 1, 0) == null);
+		assertTrue(u.deleteEmail(0, 0, 1, 1) == null);
+		assertTrue(u.deleteEmail(0, 1, 0, 0) == null);
+		assertTrue(u.deleteEmail(0, 1, 0, 1) == null);
+		assertTrue(u.deleteEmail(0, 1, 1, 0) == null);
+		assertTrue(u.deleteEmail(0, 1, 1, 1) == null);
+		assertTrue(u.deleteEmail(1, 0, 0, 1) == null);
+		assertTrue(u.deleteEmail(1, 0, 1, 0) == null);
+		assertTrue(u.deleteEmail(1, 0, 1, 1) == null);
+		assertTrue(u.deleteEmail(1, 1, 0, 0) == null);
+		assertTrue(u.deleteEmail(1, 1, 0, 1) == null);
+		assertTrue(u.deleteEmail(1, 1, 1, 0) == null);
+		assertTrue(u.deleteEmail(1, 1, 1, 1) == null);
+		
+		Email deleted = u.deleteEmail(0, 0, 0, 0);
+		assertTrue(deleted.getMessage().equals(e.getMessage()));
+		assertTrue(deleted.getSender().equals(e.getSender()));
+		assertTrue(deleted.getReceiver().equals(e.getReceiver()));
+		assertTrue(deleted.getTimeStamp().equals(e.getTimeStamp()));
+		
+		deleted = u.deleteEmail(1, 0, 0, 0);
+		assertTrue(deleted.getMessage().equals(e.getMessage()));
+		assertTrue(deleted.getSender().equals(e.getSender()));
+		assertTrue(deleted.getReceiver().equals(e.getReceiver()));
+		assertTrue(deleted.getTimeStamp().equals(e.getTimeStamp()));
+		
+		assertTrue(u.deleteEmail(0, 0, 0, 0) == null);
+		assertTrue(u.deleteEmail(1, 0, 0, 0) == null);
 	}
 	
 	@Test public void TestGetName()
